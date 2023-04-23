1a031cc88702e7309984290ef2ed030c887f78e3
BojanBardzic <b.bardzic@gmail.com>
2023-04-23 18:52:47 +0200

    Added Player class

---
+++ Slagalica/GameServer/Entities/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace GameServer.Entities
+{
+    public class Player
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+
+        public Player(string name = null)
+        {
+            Name = name;
+        }
+    }
+}
