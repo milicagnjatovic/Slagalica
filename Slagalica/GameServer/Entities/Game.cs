32dbdd1731007c83b3acf4f0b9442e4b2273ebd3
BojanBardzic <b.bardzic@gmail.com>
2023-04-23 18:53:46 +0200

    Added Game class

---
+++ Slagalica/GameServer/Entities/Game.cs
@@ -0,0 +1,23 @@
+﻿namespace GameServer.Entities
+{
+    public class Game
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public string Id { get; set; }
+        public bool InProgress { get; set; }
+
+        public Game() { 
+            Player1 = new Player();
+            Player2 = new Player();
+            InProgress = false;
+        }
+
+        public Game(Player player1, Player player2)
+        {
+            Player1 = player1;
+            Player2 = player2;
+            InProgress = false;
+        }
+    }
+}
