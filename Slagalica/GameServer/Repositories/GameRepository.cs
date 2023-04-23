3e5b89769def85452d901422c7f631e5eb4b9b85
BojanBardzic <b.bardzic@gmail.com>
2023-04-23 19:04:22 +0200

    Added game repository

---
+++ Slagalica/GameServer/Repositories/GameRepository.cs
@@ -0,0 +1,10 @@
+﻿using GameServer.Entities;
+
+namespace GameServer.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        public List<Game> Games { get; } = new List<Game>();   
+        public GameRepository() {}
+    }
+}
