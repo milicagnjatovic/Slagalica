0f4f66ff85612e22fe05c507c807f12d47ba99e6
BojanBardzic <b.bardzic@gmail.com>
2023-08-14 16:09:57 +0200

    Added new properties to player Entity

--- Slagalica/GameServer/Entities/Player.cs
+++ Slagalica/GameServer/Entities/Player.cs
@@ -4,10 +4,18 @@
     {
         public string ConnectionId { get; set; }
         public string Name { get; set; }
+        public int Points { get; set; }
+        public Dictionary<string, bool> Submitted { get; set; }
 
         public Player(string name = null)
         {
             Name = name;
+            Points = 0;
+
+            Submitted = new Dictionary<string, bool>();
+            Submitted["WhoKnowsKnows"] = false;
+            Submitted["Matches"] = false;
+            Submitted["Associations"] = false;
         }
     }
 }
